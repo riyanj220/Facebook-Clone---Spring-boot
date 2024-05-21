37c3e2809c954df60f0f95a4503b9bcaf3ec8508
Riyan Jamil <riyanjamil220@gmail.com>
2024-05-21 18:43:06 +0500

    Initial setup and added properties in application.properties

---
+++ src/test/java/com/riyanJ/facebookcloneservice/FacebookCloneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riyanJ.facebookcloneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCloneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
