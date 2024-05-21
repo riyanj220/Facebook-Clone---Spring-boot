0bf1640fc4fc307c104df4b7da3c027f6f95db9c
Riyan Jamil <riyanjamil220@gmail.com>
2024-05-21 19:56:17 +0500

    Wrote PostController and PostServiceImpl code

--- src/main/java/com/riyanJ/facebookcloneservice/controller/PostController.java
+++ src/main/java/com/riyanJ/facebookcloneservice/controller/PostController.java
@@ -1,5 +1,19 @@
 package com.riyanJ.facebookcloneservice.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riyanJ.facebookcloneservice.service.PostService;
+
+@CrossOrigin(value = "https://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/post")
 public class PostController {
 
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 }
--- src/main/java/com/riyanJ/facebookcloneservice/service/PostServiceImpl.java
+++ src/main/java/com/riyanJ/facebookcloneservice/service/PostServiceImpl.java
@@ -1,5 +1,16 @@
 package com.riyanJ.facebookcloneservice.service;
 
-public class PostServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.riyanJ.facebookcloneservice.repository.PostEntityRepository;
+
+@Service
+public class PostServiceImpl implements PostService{
+
+    private PostEntityRepository postEntityRepository;
+
+    public PostServiceImpl(PostEntityRepository postEntityRepository) {
+        this.postEntityRepository = postEntityRepository;
+    }
 
 }
